1f9dc6cedd2e665774af5a9f5e2c55ff70c263f4
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-02 21:41:17 +0800

    Create type def and init resolved for gameLibrary

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,37 @@
 // TODO: finish this and test the code below
-// const { User, Games, UserGames } = require('../models');
+const { User, GameLibrary, UserGames } = require('../models');
 // const { AuthenticationError } = require('apollo-server-express');
 // const { signToken } = require('../utils/auth');
 
-// const resolvers = {
-//     users: async () => {
-//         return User.find();
-//     },
+const resolvers = {
+    // users: async () => {
+    //     return User.find();
+    // },
 
-//     userById: async (parent, args) => {
-//         const { _id } =
-//     },
+    // userById: async (parent, args) => {
+    //     const { _id } =
+    // },
 
-//     games: async (parent, args) => {
-//         const { _id, username } = args;
+    // games: async (parent, args) => {
+    //     const { _id, username } = args;
 
-//         if (_id) {
-//             return Game.findById(_id);
-//         } else if (username) {
-//             return Game.find({ borrower: username });
-//         } else {
-//             return Game.find();
-//         }
-//     },
+    //     if (_id) {
+    //         return Game.findById(_id);
+    //     } else if (username) {
+    //         return Game.find({ borrower: username });
+    //     } else {
+    //         return Game.find();
+    //     }
+    // },
+
+    Query: {
+        gamelibrary: async () => {
+            return await GameLibrary.find();
+        },
+
+    }
 
     
-// }
+}
 
-// module.exports = resolvers;
+module.exports = resolvers;
