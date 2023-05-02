d95d0c52fac3165ff77dc08c851a7a92bcbcfad0
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-02 18:50:54 +0800

    create example data for users and games

--- server/models/User.js
+++ server/models/User.js
@@ -2,16 +2,16 @@ const  { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    username: {
+    userName: {
         type: String,
         required: true,
-        unique: true,
+        // unique: true,
         trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true,
+        // unique: true,
         match: [/.+@.+\..+/, 'Must match an email address!']
     },
     password: {
