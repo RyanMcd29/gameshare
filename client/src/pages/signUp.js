b8cc66a4b7d86dc212e78fce3c65e71715308c01
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-05-04 20:30:07 +0800

    sign up is alive

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,4 +1,130 @@
 
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// import { ADD_USER } from '../utils/mutations';
+
+// //-- Import authorization class --//
+// import Auth from '../utils/auth';
+
+// const SignUp = (props) => {
+//     const [formState, setFormState] = useState({ username: '', email: '', password: ''});
+//     const [addUser, { error, data }] = useMutation(ADD_USER);
+
+//     //-- update state based on form input changes --//
+//     const handleChange = (event) => {
+//         const { name, value } = event.target;
+
+//         setFormState({
+//             ...formState,
+//             [name]: value,
+//         });
+//     };
+
+//     //-- submit form --//
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
+//         console.log(formState);
+
+//         try {
+//             const { data } = await addUser({
+//                 variables: {...formState},
+//             });
+
+//             Auth.login(data.addUser.token);
+//         } catch (e) {
+//             console.error(e);
+//         }
+
+//         //-- If successful, clear form values --//
+//         setFormState({
+//             username: '',
+//             email: '',
+//             password: ''
+//         });
+//     };
+
+//     return (
+//         <main  className='container min-vh-100 d-flex justify-content-center align-items-center'>
+        
+//           <section className='col-6'>
+//             <div className='text-center'>
+//                 <h1>Game App!</h1>
+//                 <h6>Share Games, AnyTime, Anywhere</h6>
+//             </div>
+//             <div className="card text-center">
+//               <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+//               <div className="card-body">
+//                 {data ? (
+//                   <p>
+//                     Success! You may now head{' '}
+//                     <Link to="/">back to the homepage.</Link>
+//                   </p>
+//                 ) : (
+//                   <form onSubmit={handleFormSubmit}>
+//                     <div className='row justify-content-center m-2'>
+//                       <input
+//                         className="form-input text-center"
+//                         placeholder="Your Email..."
+//                         name="email"
+//                         type="email"
+//                         value={formState.email}
+//                         onChange={handleChange}
+//                       />
+//                     </div>
+//                     <div className='row justify-content-center m-2'>
+//                       <input
+//                         className="form-input text-center"
+//                         placeholder="Create Username..."
+//                         name="username"
+//                         type="text"
+//                         value={formState.username}
+//                         onChange={handleChange}
+//                       />
+//                     </div>
+//                     <div className='row justify-content-center m-2'>
+//                       <input
+//                         className="form-input text-center"
+//                         placeholder="Create Password..."
+//                         name="password"
+//                         type="password"
+//                         value={formState.password}
+//                         onChange={handleChange}
+//                       />
+//                     </div>
+
+//                     <button
+//                       className="row btn btn-block btn-primary"
+//                       style={{ cursor: 'pointer' }}
+//                       type="submit"
+//                     >
+//                       Sign Up!
+//                     </button>
+//                   </form>
+//                 )}
+    
+//                 {error && (
+//                   <div className="my-3 p-3 bg-danger text-white">
+//                     {error.message}
+//                   </div>
+//                 )}
+//               </div>
+//             </div>
+          
+
+//           </section>
+
+//       </main>
+//     );
+
+
+// };
+
+
+// export default SignUp;
+
+
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
@@ -7,8 +133,8 @@ import { ADD_USER } from '../utils/mutations';
 //-- Import authorization class --//
 import Auth from '../utils/auth';
 
-const SignUp = (props) => {
-    const [formState, setFormState] = useState({ username: '', email: '', password: ''});
+const SignUp = () => {
+    const [formState, setFormState] = useState({ email: '', username: '', password: ''});
     const [addUser, { error, data }] = useMutation(ADD_USER);
 
     //-- update state based on form input changes --//
@@ -33,7 +159,7 @@ const SignUp = (props) => {
 
             Auth.login(data.addUser.token);
         } catch (e) {
-            console.error(e);
+            // console.error(e);
         }
 
         //-- If successful, clear form values --//
@@ -122,4 +248,3 @@ const SignUp = (props) => {
 
 
 export default SignUp;
-
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,59 +1,110 @@
-// TODO: finish this and test the code below
+// // TODO: finish this and test the code below
+// const { User } = require('../models');
+// const { AuthenticationError } = require('apollo-server-express');
+// const { signToken } = require('../utils/auth');
+
+// const resolvers = {
+//     Query: {
+//     users: async () => {
+//       return User.find();
+//     },
+//     user: async (parent, { username }) => {
+//       return User.findOne({ username });
+//     },
+
+// // users: async () => {
+// //     return User.find();
+// // },
+
+// //     userById: async (parent, args) => {
+// //         const { _id } =
+// //     },
+
+// //     games: async (parent, args) => {
+// //         const { _id, username } = args;
+
+// //         if (_id) {
+// //             return Game.findById(_id);
+// //         } else if (username) {
+// //             return Game.find({ borrower: username });
+// //         } else {
+// //             return Game.find();
+// //         }
+// //     },
+
+//     },
+
+//     Mutation: {
+//         login: async (parent, { email, password }) => {
+//             const user = await User.findOne({ email });
+      
+//             if (!user) {
+//               throw new AuthenticationError('No user found with this email address');
+//             }
+      
+//             // const correctPw = await user.isCorrectPassword(password);
+      
+//             // if (!correctPw) {
+//             //   throw new AuthenticationError('Incorrect credentials');
+//             // }
+      
+//             const token = signToken(user);
+      
+//             return { token, user };
+//           },
+//     },
+
+//         addUser: async (parent, { username, email, password }) => {
+//           const user = await User.create({ username, email, password });
+//           const token = signToken(user);
+//           return { token, user };
+//     }
+
+    
+// }
+
+// module.exports = resolvers;
+
+
 const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
+  Query: {
     users: async () => {
       return User.find();
     },
-    user: async (parent, { userName }) => {
-      return User.findOne({ userName });
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      // const correctPw = await user.isCorrectPassword(password);
+
+      // if (!correctPw) {
+      //   throw new AuthenticationError('Incorrect credentials');
+      // }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
 
-// users: async () => {
-//     return User.find();
-// },
-
-//     userById: async (parent, args) => {
-//         const { _id } =
-//     },
-
-//     games: async (parent, args) => {
-//         const { _id, userName } = args;
-
-//         if (_id) {
-//             return Game.findById(_id);
-//         } else if (userName) {
-//             return Game.find({ borrower: userName });
-//         } else {
-//             return Game.find();
-//         }
-//     },
-
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-
-    Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            // const correctPw = await user.isCorrectPassword(password);
-      
-            // if (!correctPw) {
-            //   throw new AuthenticationError('Incorrect credentials');
-            // }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-    }
-    
-}
+  },
+};
 
 module.exports = resolvers;
