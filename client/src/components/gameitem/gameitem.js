ad8d0c9df189cefe245551b9a7853db9aa06a4bd
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-06 19:26:40 +0800

    create updates for game element state

--- client/src/components/gameitem/gameitem.js
+++ client/src/components/gameitem/gameitem.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function GameItem (game) {
-    console.log(game)
 
     const { name, image, platforms, genres } = game
 
-    console.log(platforms)
+    const [ isSelected, setSelected ] = useState(false)
+
+    const submitGame = () => {
+        console.log('hello')
+        setSelected(false)
+    }
+
+    const renderAddGameForm = () => {
+        console.log('hello')
+        setSelected(true)
+    }
+
     return (
-        <div className="card">
-            <img src={image}/>
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                { platforms.map((platform) => {
-                    return (<p key={platform} className={platform}>{platform}</p>)
-                })}
+        <div>
+            <div className="card">
+                <img src={image}/>
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    { isSelected ? (
+                        <ul>
+                            { platforms.map((platform)=>{
+                                return (<button onClick={submitGame} key={platform} className={platform}>{platform}</button>)
+                            })}
+                        </ul>)
+                    : 
+                        <button onClick={renderAddGameForm}>add this game</button>
+                    }
+                </div>
             </div>
         </div>
+        
+
+       
     )
 
 
