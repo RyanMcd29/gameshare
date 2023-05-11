037f452695d77d6c4d84740467f93f53035010b4
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-11 23:48:00 +0800

    add dotenv

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
-const secret = 'super secret thingy';
+const secret = process.env.REACT_APP_AUTH_KEY;
 const expiration = '2h';
 
 
