795f3f559b14da23dd3d297431d6a8540a2b0603
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-06-18 18:46:39 +0800

    logout error has been fixed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,3 +1,89 @@
+// import React from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import Auth from '../utils/auth';
+
+// const Header = () => {
+//   const location = useLocation();
+
+//   //-- User logout --//
+//   const logout = () => {
+//     Auth.logout();
+//   };
+
+
+//   return (
+//     <header className="headerColor sticky-top">
+//       <div className="d-flex justify-content-center align-items-center">
+//         <div className="col-3 text-start">
+//           <Link to="/homepage" style={{ textDecoration: 'none' }}>
+//             <h1 className="headerTitle ms-2 mt-2">GameShare.</h1>
+//           </Link>
+//         </div>
+//         <div className="col-auto"></div>
+//         <div className="col text-end me-2">
+//           {Auth.loggedIn() ? (
+//             <>
+//               <Link to="/homepage" className='userNameHeader me-2'> 
+//                 {Auth.getProfile().data.username}
+//               </Link>
+//               {location.pathname !== '/homepage' && location.pathname !== '/' && (
+//                 <Link to="/homepage">
+//                   <button className="button-80 ms-1" type="button">
+//                     Dashboard
+//                   </button>
+//                 </Link>
+//               )}
+//               {location.pathname !== '/inbox' && location.pathname !== '/' && (
+//                 <Link to="/inbox">
+//                   <button className="button-80 ms-1" type="button">
+//                     Inbox
+//                   </button>
+//                 </Link>
+//               )}
+//                 <Link to="/login">
+//                   <button
+//                     className="button-80 ms-1"
+//                     type="button"
+//                     onClick={logout}
+//                   >
+//                     Logout
+//                   </button>
+//                 </Link>
+//               {location.pathname === '/signup' && (
+//                 <Link to="/login">
+//                   <button className="button-80 ms-1" type="button">
+//                     Login
+//                   </button>
+//                 </Link>
+//               )}
+//             </>
+//           ) : location.pathname !== '/' && (
+//             <>
+//               {location.pathname !== '/login' && (
+//                 <Link to="/login">
+//                   <button className="button-80 ms-1" type="button">
+//                     Login
+//                   </button>
+//                 </Link>
+//               )}
+//               {location.pathname !== '/signup' && (
+//                 <Link to="/signup">
+//                   <button className="button-80 ms-1" type="button">
+//                     Signup
+//                   </button>
+//                 </Link>
+//               )}
+//             </>
+//           )}
+          
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Auth from '../utils/auth';
@@ -5,11 +91,9 @@ import Auth from '../utils/auth';
 const Header = () => {
   const location = useLocation();
 
-  //-- User logout --//
   const logout = () => {
     Auth.logout();
   };
-
 
   return (
     <header className="headerColor sticky-top">
@@ -40,15 +124,13 @@ const Header = () => {
                   </button>
                 </Link>
               )}
-                <Link to="/login">
-                  <button
-                    className="button-80 ms-1"
-                    type="button"
-                    onClick={logout}
-                  >
-                    Logout
-                  </button>
-                </Link>
+              <button
+                className="button-80 ms-1"
+                type="button"
+                onClick={logout}
+              >
+                Logout
+              </button>
               {location.pathname === '/signup' && (
                 <Link to="/login">
                   <button className="button-80 ms-1" type="button">
@@ -75,7 +157,6 @@ const Header = () => {
               )}
             </>
           )}
-          
         </div>
       </div>
     </header>
